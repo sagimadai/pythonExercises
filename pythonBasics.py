4ceeb97a4ff2e6257dce5f9031f21d78e71f194b
sagimadai <madsagi11@gmail.com>
2025-04-22 08:35:57 +0000

    first exercise

---
+++ pythonBasics.py
@@ -0,0 +1,16 @@
+#Exercise 1
+
+sentence = input("please enter your sentence ")
+length = len(sentence)
+print(f"The lenght of your sentence is {length}")
+
+#Exercise 2
+
+favorite_color = "blue"
+age = 28
+favorite_animal = "dog"
+
+#Why are these variable names good?
+#1.Descriptive and meaningful
+#2.Written in English
+#3.Follow Python naming rules
